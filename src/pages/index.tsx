0ca814e95f88485e5b1c80a959af52bd5a9fc18e
Joshua Jackson <skate.jackj@gmail.com>
2021-12-16 23:02:53 -0500

    small style changes and small change to music page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,13 +12,13 @@ const categories = [
 
 export default function Home() {
   return (
-    <div className="text-center">
+    <div className="text-center px-4">
       <span>This is the interface to all information related to Shmoji.</span>
-      <div className="md:w-3/5 mx-auto flex justify-between mt-6">
+      <div className="md:w-3/5 mx-auto flex flex-wrap justify-between mt-6">
         {categories.map(category => {
           return (
             <A href={category.href} key={category.name}>
-              <button className="p-3 bg-blue-600 text-white rounded">{category.name}</button>
+              <button className="p-3 mb-4 mr-2 bg-blue-600 text-white rounded">{category.name}</button>
             </A>
           )
         })}
--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -10,8 +10,8 @@ const music = () => {
         <span><A href="https://www.youtube.com/redirect?event=channel_banner&redir_token=QUFFLUhqay1PSnpYcWRYZ2E2ZVROSlJYRWZtS3l4SzJSUXxBQ3Jtc0treHZMRGRYRktISnpDcWxvY09zZ1N3bnhtLWhjV0JOMlZ3Mm1xWEpSY1VlOEllMkRYeWQ0UnFIMmhNMVhnaHJ5d3ZKaGVZdUY1WDVLMFpUM3BNNXNSR2pUanREeXExcHFlTHJZTmp1ZmxVdndGbGIwcw&q=https%3A%2F%2Fopen.spotify.com%2Fartist%2F7Cs2wbxIpHRF4w30nC2cY8%3Fsi%3DaxVkrOjYSZSk2dmUCPJ6uQ">Music on Spotify</A></span>
       </div>
       <div className="text-center">
-        <span>Shmoji&apos;s only music video:</span>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/0jtcREYb57w" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        <span>Shmoji&apos;s personal fav:</span>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/wxqtn_tCPdM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
       </div>
     </div>
   )
