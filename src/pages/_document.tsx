f44a6393958d15a0dbc569109562a863c45811f7
Joshua Jackson <skate.jackj@gmail.com>
2022-02-05 09:26:48 -0500

    fixed bug with document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,29 +1,44 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        {/* Global site tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={"https://www.googletagmanager.com/gtag/js?id=G-H799XFX8G4"}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-H799XFX8G4');
-            `,
-          }}
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render () {
+    return (
+      <Html>
+        <Head>
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={"https://www.googletagmanager.com/gtag/js?id=G-H799XFX8G4"}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-H799XFX8G4');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
