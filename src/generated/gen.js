030a8acb6417bb735ce2e3e05d4e9fecae0a8142
Joshua Jackson <skate.jackj@gmail.com>
2023-08-06 11:15:34 -0400

    disabled ar and vr for now

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,14 +2,14 @@
 
 export const needle_exported_files = [];
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("./assets/sceneRoot.glb?v=1691331436250");
+needle_exported_files.push("./assets/sceneRoot.glb?v=1691334687668");
 
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1691331436250");
+		needleEngine.setAttribute("hash", "1691334687668");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
