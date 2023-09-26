c709ac837bc56fe8c1be4455aeb619b53db5d3a8
Joshua Jackson <skate.jackj@gmail.com>
2023-09-26 13:09:25 -0400

    testing if embed is issue

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,14 +2,14 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("./assets/fpsExperiment.glb?v=1695739211502");
+needle_exported_files.push("./assets/fpsExperiment.glb?v=1695748122429");
 
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1695739211502");
+		needleEngine.setAttribute("hash", "1695748122429");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
