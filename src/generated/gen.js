6344022ae2bb0f2081cce438971cd4d580b8d9b8
Joshua Jackson <skate.jackj@gmail.com>
2023-09-26 13:56:36 -0400

    guh huh 2

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,14 +2,14 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("./assets/fpsExperiment.glb?v=1695749281459");
+needle_exported_files.push("./assets/fpsExperiment.glb?v=1695750888439");
 
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1695749281459");
+		needleEngine.setAttribute("hash", "1695750888439");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
