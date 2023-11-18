f14afd26abc684c9d6390c0b647e71e280759b5e
Joshua Jackson <skate.jackj@gmail.com>
2023-11-18 13:00:56 -0500

    removed some audio thing from camera...weird

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,14 +2,14 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("./assets/fpsExperiment.glb?v=1700329564176");
+needle_exported_files.push("./assets/fpsExperiment.glb?v=1700330409863");
 
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1700329564176");
+		needleEngine.setAttribute("hash", "1700330409863");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
