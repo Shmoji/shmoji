736c45adfc94c6d407f3fad5325307fddb377584
Joshua Jackson <skate.jackj@gmail.com>
2023-11-18 12:46:56 -0500

    no longer using sample physics material...woops

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,14 +2,14 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("./assets/fpsExperiment.glb?v=1700327530694");
+needle_exported_files.push("./assets/fpsExperiment.glb?v=1700329564176");
 
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1700327530694");
+		needleEngine.setAttribute("hash", "1700329564176");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
