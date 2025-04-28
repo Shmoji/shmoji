a7533f19ee97842c7a890a8a9c6a3393c7e26c74
Joshua Jackson <skate.jackj@gmail.com>
2025-04-28 16:48:16 +0900

    new fav song of mine

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -11,7 +11,7 @@ const music = () => {
       </div>
       <div className="text-center">
         <span>Shmoji&apos;s personal fav:</span>
-        <iframe className="max-w-full" width="560" height="315" src="https://www.youtube.com/embed/t0exXgFnqJM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        <iframe className="max-w-full" width="560" height="315" src="https://www.youtube.com/embed/Rghcd1td2Qs?si=WGOYD_mO7gTDxk4K" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
       </div>
     </div>
   )
